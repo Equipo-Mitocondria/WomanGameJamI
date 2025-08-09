ab2cb91c7bec6869e170848a31e4e3ff3056bb08
PabloSanchezDV <paudev63@gmail.com>
2025-08-09 17:20:44 +0200

    👷 Building first prototype scene
    · Created some SanityEffects
    · Refatorized some systems to connect logic

--- WomanGameJamMitocondria/Assets/Core/Scripts/Character/States/ExploringState.cs
+++ WomanGameJamMitocondria/Assets/Core/Scripts/Character/States/ExploringState.cs
@@ -1,5 +1,8 @@
+using System.Collections;
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 
 public class ExploringState : FSMTemplateState
 {
@@ -23,7 +26,11 @@ public class ExploringState : FSMTemplateState
         _inputActions.Exploring.Enable();
     }
 
-    public override void UpdateLogic() { }
+    public override void UpdateLogic() 
+    {
+        if(((Character)_fsm).IsWorking)
+            ((Character)_fsm).ChangeState(((Character)_fsm).workingState);
+    }
 
     public override void UpdatePhysics() 
     {
@@ -50,7 +57,12 @@ public class ExploringState : FSMTemplateState
     {
         _movementV2 = _movement.ReadValue<Vector2>();
 
-        _rb.AddForce(CameraManager.Instance.ActiveCamera.transform.forward * ((Character)_fsm).MovementForce, ForceMode.Impulse);
+        Vector3 projectedForward = ProjectVectorOnXZPlane(CameraManager.Instance.ActiveCamera.transform.forward);
+        Vector3 projectedRight = ProjectVectorOnXZPlane(CameraManager.Instance.ActiveCamera.transform.right);
+
+        RotatePlayer(projectedForward, projectedRight, _movementV2.x, _movementV2.y);
+
+        _rb.AddForce((projectedForward * _movementV2.y + projectedRight * _movementV2.x) * ((Character)_fsm).MovementForce * Time.deltaTime, ForceMode.Impulse);
 
         CapVelocity(_rb);
     }
@@ -58,11 +70,56 @@ public class ExploringState : FSMTemplateState
     private void CapVelocity(Rigidbody rb)
     {
         if (rb.linearVelocity.magnitude > ((Character)_fsm).MaxVelocity)
-        {
-            rb.linearVelocity.Normalize();
-            rb.linearVelocity *= ((Character)_fsm).MaxVelocity;
-        }
+            rb.linearVelocity = ((Character)_fsm).MaxVelocity * rb.linearVelocity.normalized;
         else if(rb.linearVelocity.magnitude < Mathf.Epsilon)
             rb.linearVelocity = Vector3.zero;
+    }
+
+    private Vector3 ProjectVectorOnXZPlane(Vector3 vector)
+    {
+        Vector3 vectorProjected = new Vector3(vector.x, 0f, vector.z);
+        return vectorProjected.normalized;
+    }
+
+    private void RotatePlayer(Vector3 forward, Vector3 right, float horizontalInput, float verticalInput)
+    {
+        if (!(Mathf.Abs(horizontalInput) > Mathf.Epsilon || Mathf.Abs(verticalInput) > Mathf.Epsilon))
+            return;
+
+        // North, South, East & West rotations
+        if (Mathf.Abs(horizontalInput) > Mathf.Abs(verticalInput))
+        {
+            if (horizontalInput < 0f)
+                _fsm.transform.rotation = Quaternion.LookRotation(-right);
+            else if (horizontalInput > 0f)
+                _fsm.transform.rotation = Quaternion.LookRotation(right);
+        }
+        else if(Mathf.Abs(horizontalInput) < Mathf.Abs(verticalInput))
+        {
+            if (verticalInput < 0f)
+                _fsm.transform.rotation = Quaternion.LookRotation(-forward);
+            else if (verticalInput > 0f)
+                _fsm.transform.rotation = Quaternion.LookRotation(forward);
+        }
+        else if(Mathf.Abs(horizontalInput) == Mathf.Abs(verticalInput))
+        {
+            //S
+            if (verticalInput < 0f)
+            {
+                //SW
+                if(horizontalInput < 0f)
+                    _fsm.transform.rotation = Quaternion.LookRotation((-right - forward).normalized);
+                else if (horizontalInput > 0f) //SE
+                    _fsm.transform.rotation = Quaternion.LookRotation((right - forward).normalized);
+            }
+            else if(verticalInput > 0f)
+            {
+                //NW
+                if (horizontalInput < 0f)
+                    _fsm.transform.rotation = Quaternion.LookRotation((-right + forward).normalized);
+                else if (horizontalInput > 0f) //NE
+                    _fsm.transform.rotation = Quaternion.LookRotation((right + forward).normalized);
+            }
+        }
     }
 }
--- WomanGameJamMitocondria/Assets/Core/Scripts/Work.cs
+++ WomanGameJamMitocondria/Assets/Core/Scripts/Work.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class Work : MonoBehaviour
 {
+    [SerializeField] private SanityEffect _sanityEffect;
     [SerializeField] private float _workIncreaseSpeed;
     [SerializeField] private float _maxWork;
     private float _currentWork;
-
     private bool _isWorking;
+
+    public float CurrentWorkAmount { get { return _currentWork; } }
     public bool IsWorking {  get { return _isWorking; } set { _isWorking = value; } }
 
     private void Start()
@@ -18,6 +20,8 @@ public class Work : MonoBehaviour
     {
         if (_isWorking)
         {
+            Sanity.Instance.ApplySanityEffect(_sanityEffect, true);
+
             _currentWork += _workIncreaseSpeed * Time.deltaTime;
 
             if(_currentWork >= _maxWork)
